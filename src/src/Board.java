740b3a5c1517fb2db406086edda6b1c3ef261059
navkamboj <navjotkamboj0206@gmail.com>
2024-02-03 20:41:47 -0500

    Intial setup

--- src/src/Board.java
+++ src/src/Board.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Board {
+/**
+ * Navjot Kamboj- 40240781 and Yatish Chutani -40266553
+ * COMP
+ * Assignment #1 (Part-II)
+ * Due Date: 11th Feb, 2024
+ *
+ */
+public class Board {
 }
